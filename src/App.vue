7ed8fbb21297d46e49d07232fedc4e64dd56132f
Matt Leicht <mattleicht@protonmail.com>
2023-01-18 16:17:47 -0600

    change to test deployment

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
+  <NavigationHeader/>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome"/>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import NavigationHeader from './components/NavigationHeader.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,NavigationHeader
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+body{
+
 }
 </style>
---
+++ src/components/NavigationHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>Hali Technology Demonstrations</div>
+    <div>About</div>
+    <div>Holder-1</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
